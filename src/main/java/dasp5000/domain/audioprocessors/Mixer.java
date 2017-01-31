636abbcf536bb755d9d81747cc2ec8b318507dd3
Petri Kallio <kallionpetri@gmail.com>
2017-01-31 12:34:18 +0200

    Signal mixer started

---
+++ src/main/java/dasp5000/domain/audioprocessors/Mixer.java
@@ -0,0 +1,77 @@
+
+package dasp5000.domain.audioprocessors;
+
+import dasp5000.domain.AudioContainer;
+import dasp5000.domain.DynamicArray;
+
+/**
+ * Mixes the signals together
+ * 
+ * @author Petri Kallio
+ */
+public class Mixer implements AudioProcessor {
+    private DynamicArray<AudioContainer> audioContainers;
+    private AudioContainer mix;
+    private long mixLength;
+    private int bitsPerSample;
+
+    public Mixer(AudioContainer... audioContainers) {
+        if (audioContainers.length == 0) {
+            throw new IllegalArgumentException("No AudioContainers supplied");
+        }
+        this.mixLength = 0;
+        this.bitsPerSample = audioContainers[0].getBitDepth();
+        this.audioContainers = new DynamicArray<>(AudioContainer.class);
+        for (int i = 0; i < audioContainers.length; i++) {
+            AudioContainer ac = audioContainers[i];
+            if (ac.getBitDepth() != this.bitsPerSample) {
+                throw new IllegalArgumentException("The AudioContainers' "
+                        + "sample resolution must match");
+            }
+            this.audioContainers.add(ac);
+            long samples = ac.getAudioAnalysis().getSamples();
+            if (samples > this.mixLength) {
+                this.mixLength = samples;
+            }
+        }
+        this.mixLength = longestAudio();
+    }
+    
+    public void addAudioContainer(AudioContainer audioContainer) {
+        this.audioContainers.add(audioContainer);
+        this.mixLength = longestAudio();
+    }
+
+    @Override
+    public void process() {
+        DynamicArray<Integer> wordMix = new DynamicArray<>(Integer.class);
+        int maxSample = (int)Math.pow(2, bitsPerSample) / 2;
+        for (long i = 0; i < mixLength; i++) {
+            double mix = 0;
+            for (int j = 0; j < audioContainers.size(); j++) {
+                AudioContainer ac = audioContainers.get(j);
+                if (ac.getAudioAnalysis().getSamples() > i) {
+                    double sample = sampleToDouble(ac.getWords().get((int)i), 
+                            maxSample);
+                    mix = mix + sample - mix * sample;
+                }
+            }
+            int mixedSample = doubleToSample(mix);
+        }
+    }
+
+    private long longestAudio() {
+        long maxLength = 0;
+        for (int i = 0; i < audioContainers.size(); i++) {
+            long samples = audioContainers.get(i).getAudioAnalysis().getSamples();
+            if (samples > maxLength) {
+                maxLength = samples;
+            }
+        }
+        return maxLength;
+    }
+    
+    private double sampleToDouble(int sample, int max) {
+        return sample / max;
+    }
+}
